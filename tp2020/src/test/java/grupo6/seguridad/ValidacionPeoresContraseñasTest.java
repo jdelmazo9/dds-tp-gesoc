3ab416d6f03864ad1ec61fcb4ac25ce2f01e4f4d
mzuccotti <martin_zuc@hotmail.com>
2020-05-03 20:51:33 -0300

    Arreglo

--- tp2020/src/test/java/grupo6/seguridad/ValidacionPeoresContraseñasTest.java
+++ tp2020/src/test/java/grupo6/seguridad/ValidacionPeoresContraseñasTest.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ValidacionPeoresContraseñasTest {
 
-    Validacion worstPasswordsVal;
+    Validacion validacionPeoresContraseñas;
 
     @BeforeEach
     void setUp() {
-        worstPasswordsVal = new ValidacionPeoresContraseñas();
+        validacionPeoresContraseñas = new ValidacionPeoresContraseñas();
     }
 
     @Test
     void peoresContraseñasTest(){
-        assertFalse(worstPasswordsVal.validar("midnight"), "La contraseña se encuentra en el top 10k de peores contraseñas");
-        assertFalse(worstPasswordsVal.validar("987654"), "La contraseña se encuentra en el top 10k de peores contraseñas");
-        assertTrue(worstPasswordsVal.validar("midn$ght"), "La contraseña es valida");
+        assertFalse(validacionPeoresContraseñas.validar("midnight"), "La contraseña se encuentra en el top 10k de peores contraseñas");
+        assertFalse(validacionPeoresContraseñas.validar("987654"), "La contraseña se encuentra en el top 10k de peores contraseñas");
+        assertTrue(validacionPeoresContraseñas.validar("midn$ght"), "La contraseña es valida");
     }
 }
