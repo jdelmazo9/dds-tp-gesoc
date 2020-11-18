b36239a828bca2eee745bf41670ca04b3c9487dd
jdelmazo <j.delmazo@hotmail.com>
2020-11-18 02:30:42 -0300

    ya ksi breo

--- tp2020/src/main/java/grupo6/server/Server.java
+++ tp2020/src/main/java/grupo6/server/Server.java
@@ -3,10 +3,46 @@ package grupo6.server;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Server {
     public static void main(String[] args) {
-        Spark.port(9000);
+        //Spark.port(9000);
+        Spark.port(getHerokuAssignedPort());
+        overridePersistence();
         Router.init();
         DebugScreen.enableDebugScreen();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 9000; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    static void overridePersistence() {
+        Map<String, String> env = System.getenv();
+        Map<String, Object> configOverrides = new HashMap<String, Object>();
+        for (String envName : env.keySet()) {
+            if (envName.contains("USER_DB")) {
+                configOverrides.put("hibernate.connection.username", env.get(envName));
+            }
+            if (envName.contains("PASS_DB")) {
+                configOverrides.put("hibernate.connection.password", env.get(envName));
+            }
+            if (envName.contains("URL_DB")) {
+                configOverrides.put("hibernate.connection.url", env.get(envName));
+            }
+            // You can put more code in here to populate configOverrides...
+        }
+
+        EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("default", configOverrides);
+
     }
 }
--- tp2020/src/test/java/grupo6/dominio/ConnTest.java
+++ tp2020/src/test/java/grupo6/dominio/ConnTest.java
@@ -1,57 +1,57 @@
-package grupo6.dominio;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import grupo6.seguridad.RolUsuario;
-import grupo6.seguridad.Usuario;
-
-import java.sql.*;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class ConnTest {
-
-    private static EntityManagerFactory entityManagerFactory; 
-
-    @BeforeClass
-    public static void setUpClass() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("db");
-    }
-    
-    @Test
-    public void testConn() throws Exception {
-        final String DB_CONNECTION = "jdbc:mysql://localhost:3306/dds";
-        final String DB_USER = "root";
-        final String DB_PASSWORD = "100296";
-        Connection dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,	DB_PASSWORD);
-
-        Statement statement = dbConnection.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT PersonID, LastName from Persons");
-        while (rs.next()) {
-            Integer id = rs.getInt("PersonID");
-            String prop = rs.getString("LastName");
-            System.out.println(id);
-            System.out.println(prop);
-        }
-
-    }
-
-    @Test
-    public void creaTablaUsuario(){
-
-        EntityManager entityManager;
-
-        entityManager = entityManagerFactory.createEntityManager();
-
-        entityManager.getTransaction().begin();
-
-        Usuario yo = new Usuario("yo", "ashgfajf093", RolUsuario.ADMIN);
-
-        entityManager.persist(yo);
-        entityManager.getTransaction().commit();
-
-    }
-}
+//package grupo6.dominio;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import grupo6.seguridad.RolUsuario;
+//import grupo6.seguridad.Usuario;
+//
+//import java.sql.*;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.Persistence;
+//
+//public class ConnTest {
+//
+//    private static EntityManagerFactory entityManagerFactory;
+//
+//    @BeforeClass
+//    public static void setUpClass() {
+//        entityManagerFactory = Persistence.createEntityManagerFactory("db");
+//    }
+//
+//    @Test
+//    public void testConn() throws Exception {
+//        final String DB_CONNECTION = "jdbc:mysql://localhost:3306/dds";
+//        final String DB_USER = "root";
+//        final String DB_PASSWORD = "100296";
+//        Connection dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,	DB_PASSWORD);
+//
+//        Statement statement = dbConnection.createStatement();
+//        ResultSet rs = statement.executeQuery("SELECT PersonID, LastName from Persons");
+//        while (rs.next()) {
+//            Integer id = rs.getInt("PersonID");
+//            String prop = rs.getString("LastName");
+//            System.out.println(id);
+//            System.out.println(prop);
+//        }
+//
+//    }
+//
+//    @Test
+//    public void creaTablaUsuario(){
+//
+//        EntityManager entityManager;
+//
+//        entityManager = entityManagerFactory.createEntityManager();
+//
+//        entityManager.getTransaction().begin();
+//
+//        Usuario yo = new Usuario("yo", "ashgfajf093", RolUsuario.ADMIN);
+//
+//        entityManager.persist(yo);
+//        entityManager.getTransaction().commit();
+//
+//    }
+//}
